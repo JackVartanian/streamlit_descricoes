96e346a098aa19823c481acf954024071cbdc6d7
JackVartanian <76970892+JackVartanian@users.noreply.github.com>
2024-08-06 16:00:31 -0300

    Acompanhamento de Dados Prods Shopfy

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import io
+#importar função de uma pasta no nível acima
+import sys
+import os
+
+st.set_page_config(
+    page_title="Acompanhameto de Produtos",
+    page_icon="💎",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.title("Acompanhamento de Produtos")
+st.divider()
+
+col1, col2, col3 = st.columns([1, 1, 1])
+
+with col1:
+
+    st.text("Produtos sem descrição:")
+    df = pd.read_excel('arquivos/dados_prods_.xlsx')
+    df = df[df['descriptionHtml'].isnull()]
+    df = df[df['totalInventory'] > 0]
+    df = df[['descriptionHtml', 'sku', 'title']]
+    df = df.rename(columns={'descriptionHtml': 'Descrição', 'sku': 'SKU', 'title': 'Título'})
+    df = df.reset_index(drop=True)
+    st.dataframe(df)
+    
+with col2:
+    
+    st.text("Produtos sem Imagem:")
+    df = pd.read_excel('arquivos/dados_prods_.xlsx')
+    df = df[df['mediaCount.count'] == 0]
+    df = df[df['totalInventory'] > 0]
+    df = df[['mediaCount.count', 'sku', 'title']]
+    df = df.rename(columns={'mediaCount.count': 'Imagens', 'sku': 'SKU', 'title': 'Título'})
+    df = df.reset_index(drop=True)
+    st.dataframe(df)
+    
+with col3:
+    
+    st.text("Produtos sem Composição:")
+    df = pd.read_excel('arquivos/dados_prods_.xlsx')
+    df = df[df['metafield.value'].isnull()]
+    df = df[df['totalInventory'] > 0]
+    df = df[['metafield.value', 'sku', 'title']]
+    df = df.rename(columns={'metafield.value': 'Composição', 'sku': 'SKU', 'title': 'Título'})
+    df = df.reset_index(drop=True)
+    st.dataframe(df)
---
+++ executar.bat
@@ -0,0 +1,2 @@
+@REM cd C:\Users\Thiago.Almeida\OneDrive - Jack Vartanian\Python\Manipulacoes_Thiago\streamlit_descricoes
+streamlit run app.py
